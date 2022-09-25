05b9c11cbfa61fa39889322bdcf3f21d45115ad9
dippp <Dipppp84@gmail.com>
2022-09-26 02:31:55 +0300

    feat: add serverless and plugin

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -18,6 +18,7 @@ test("Renders products list", async () => {
       description: "Product 1 description",
       price: 1,
       count: 1,
+      image: ""
     },
     {
       id: "2",
@@ -25,7 +26,8 @@ test("Renders products list", async () => {
       description: "Product 2 description",
       price: 2,
       count: 2,
-    },
+      image: ""
+    }
   ];
   server.use(
     rest.get(`${API_PATHS.bff}/product/available`, (req, res, ctx) => {
